7d878ee1d32a06580a7ba0edf0e71da758cb03d7
Vishnu Kalyan <vishnukalyan77@gmail.com>
2019-07-10 21:26:12 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'whatsapp_home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "WhatsApp",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff075E54),
+        accentColor: Color(0xff25D366),
+      ),
+      home: WhatsAppHome(),
+    );
+  }
+}
---
+++ models/chat_model.dart
@@ -0,0 +1,60 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Vishnu Kalyan",
+    message: "Hello",
+    time: "04:26",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "Vishnu",
+    message: "Hello all",
+    time: "04:27",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "Kalyan",
+    message: "Hell",
+    time: "04:28",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "once",
+    message: "whatsup!",
+    time: "04:29",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "Name",
+    message: "Toes are in",
+    time: "04:30",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "Raj",
+    message: "Boolam",
+    time: "04:31",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+  new ChatModel(
+    name: "Vk",
+    message: "77",
+    time: "04:32",
+    avatarUrl:
+        "https://www.google.com/search?q=harry+potter+images&rlz=1C1CHBD_enIN801IN801&tbm=isch&source=iu&ictx=1&fir=asnaJJzlU7N-uM%253A%252CDsehuVZzuQhBBM%252C_&vet=1&usg=AI4_-kRr3ovER9tShBrj2j-Q-_qM8nvdPQ&sa=X&ved=2ahUKEwjR1u7mt8_hAhUMfysKHWwkCXYQ9QEwA3oECAcQCg#",
+  ),
+];
